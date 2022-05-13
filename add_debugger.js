c24ffc4a9a4e9959108fb6e96963801e3e8cd42f
Filip Petrovic <filip.petroviccc@gmail.com>
2022-05-13 22:25:43 +0200

    changed page name

--- add_debugger.js
+++ add_debugger.js
@@ -1,6 +1,6 @@
 if (typeof initDebugger === 'undefined') {
     const initDebugger = function () {
-        const tenderlyRegex  = new RegExp('dashboard.tenderly.co/tx')
+        const tenderlyRegex = new RegExp('dashboard.tenderly.co/tx')
         if (document.body.innerHTML.match(tenderlyRegex)) {
             return
         }
@@ -13,12 +13,9 @@ if (typeof initDebugger === 'undefined') {
         const htmlElementRegex = new RegExp('(?:</[^<]+>)|(?:<[^<]+/>)')
 
         const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
-        for (let i=0;i < text.length; i++) {
+        for (let i = 0; i < text.length; i++) {
             if (!htmlElementRegex.test(text[i].innerHTML) && text[i].innerHTML.search(regex) === 0) {
-                text[i].innerHTML = text[i].innerHTML.replace(
-                    regex,
-                    '0x$1 <a href="https://dashboard.tenderly.co/tx/mainnet/0x$1/debugger">View in Tenderly!</a>'
-                )
+                text[i].innerHTML = text[i].innerHTML.replace(regex, '0x$1 <a href="https://dashboard.tenderly.co/tx/0x$1/one-click-debugger">View in Tenderly!</a>')
             }
         }
     }

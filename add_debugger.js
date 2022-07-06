bba1c23b76ba0a2c731b28231a7da3b8860cae23
belactriple9 <carippey1@gmail.com>
2022-07-06 09:40:10 -0400

    Add a way to check if the data exists on the blockchain

--- add_debugger.js
+++ add_debugger.js
@@ -15,7 +15,29 @@ if (typeof initDebugger === 'undefined') {
         const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
         for (let i = 0; i < text.length; i++) {
             if (!htmlElementRegex.test(text[i].innerHTML) && text[i].innerHTML.search(regex) === 0) {
-                text[i].innerHTML = text[i].innerHTML.replace(regex, '0x$1 <a href="https://dashboard.tenderly.co/tx/0x$1/one-click-debugger">View in Tenderly!</a>')
+                // call the RPC to see if the data is a real transaction
+                const transactionReceipt = await fetch('https://cloudflare-eth.com/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        jsonrpc: '2.0',
+                        method: 'eth_getTransactionReceipt',
+                        params: [text[i].innerHTML],
+                        id: 1
+                    })
+                });
+                // get the data from the response
+                const transactionReceiptJSON = await transactionReceipt.json();
+
+                if(transactionReceipt.status === 200 && transactionReceiptJSON.result !== null) {
+                    text[i].innerHTML = text[i].innerHTML.replace(regex, '0x$1 <a href="https://dashboard.tenderly.co/tx/0x$1/one-click-debugger">View in Tenderly!</a>')
+                }
+                else if(transactionReceipt.status != 200) {
+                    text[i].innerHTML = text[i].innerHTML.replace(regex, '0x$1 <a href="https://dashboard.tenderly.co/tx/0x$1/one-click-debugger">View in Tenderly!</a>')
+                }
+                // else do nothing
             }
         }
     }

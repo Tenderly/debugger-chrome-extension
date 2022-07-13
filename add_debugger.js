656b50d3004c7b406252ee647bbd065fe038704a
Filip Petrović <filippetroviccc@users.noreply.github.com>
2022-07-13 20:26:32 +0200

    updated regex pattern, tenderly logo and new open in new window (#4)

--- add_debugger.js
+++ add_debugger.js
@@ -5,17 +5,13 @@ if (typeof initDebugger === 'undefined') {
             return
         }
 
-        const injectElement = document.createElement('div')
-        injectElement.className = 'debuggerTest'
-        injectElement.innerHTML = 'View in Tenderly'
-
-        const regex = new RegExp('0x([a-f0-9]{64})')
+        const regex = new RegExp('^0x([a-fA-F0-9]{64})$')
         const htmlElementRegex = new RegExp('(?:</[^<]+>)|(?:<[^<]+/>)')
 
         const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
         for (let i = 0; i < text.length; i++) {
             if (!htmlElementRegex.test(text[i].innerHTML) && text[i].innerHTML.search(regex) === 0) {
-                text[i].innerHTML = text[i].innerHTML.replace(regex, '0x$1 <a href="https://dashboard.tenderly.co/tx/0x$1/one-click-debugger">View in Tenderly!</a>')
+                text[i].innerHTML = text[i].innerHTML.replace(regex, '0x$1 <a href="https://dashboard.tenderly.co/tx/0x$1/one-click-debugger" target="_blank">View in Tenderly <img src="https://storage.googleapis.com/tenderly-public-assets/Tenderly-logo-symbol.png" width="20px"/></a>')
             }
         }
     }
